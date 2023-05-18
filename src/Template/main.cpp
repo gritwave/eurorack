ea1ff107bc3610683149f00237bfde1b8a311754
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-05-18 13:52:45 +0200

    Add pre-commit hooks

--- src/Template/main.cpp
+++ src/Template/main.cpp
@@ -26,24 +26,22 @@ DaisyPatchSM patch;
  *
  *  This size is acceptable for many applications, and provides an extremely low
  * latency from input to output. However, you can change this size by calling
- * patch.SetAudioBlockSize(desired_size). When running complex DSP it can be more
- * efficient to do the processing on larger chunks at a time.
+ * patch.SetAudioBlockSize(desired_size). When running complex DSP it can be
+ * more efficient to do the processing on larger chunks at a time.
  *
  */
-void AudioCallback(AudioHandle::InputBuffer  in,
-                   AudioHandle::OutputBuffer out,
-                   size_t                    size)
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
     /** The easiest way to do pass thru is to simply copy the input to the output
-   * In C++ the standard way of doing this is with std::copy. However, those
-   * familliar with C can use memcpy. A simple loop is also a good way to do
-   * this.
-   *
-   * Since you'll most likely want to be doing something between the input,
-   *  and the output, and not just passing it through we'll demonstrate doing
-   *  so with a for loop.
-   */
-    for(size_t i = 0; i < size; i++)
+     * In C++ the standard way of doing this is with std::copy. However, those
+     * familliar with C can use memcpy. A simple loop is also a good way to do
+     * this.
+     *
+     * Since you'll most likely want to be doing something between the input,
+     *  and the output, and not just passing it through we'll demonstrate doing
+     *  so with a for loop.
+     */
+    for (size_t i = 0; i < size; i++)
     {
         out[0][i] = in[0][i]; /**< Copy the left input to the left output */
         out[1][i] = in[1][i]; /**< Copy the right input to the right output */
@@ -57,5 +55,5 @@ int main(void)
 
     /** Start Processing the audio */
     patch.StartAudio(AudioCallback);
-    while(1) {}
+    while (1) {}
 }
