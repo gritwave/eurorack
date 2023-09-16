c49a85d8b6130a157836254e1e5b89ebc0746c97
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-09-16 22:15:55 +0200

    Add tests for WhiteNoise

--- lib/main_test.cpp
+++ lib/main_test.cpp
@@ -5,6 +5,7 @@ extern auto test_decibel() -> bool;
 extern auto test_fft() -> bool;
 extern auto test_ilog2() -> bool;
 extern auto test_ipow() -> bool;
+extern auto test_white_noise() -> bool;
 
 auto main() -> int
 {
@@ -12,5 +13,6 @@ auto main() -> int
     assert(test_fft());
     assert(test_ilog2());
     assert(test_ipow());
+    assert(test_white_noise());
     return 0;
 }
