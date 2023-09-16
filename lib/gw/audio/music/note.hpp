57fe1669d758f7e5dcbd7dc79da05e600c97dbd4
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-09-16 22:25:19 +0200

    Cleanup noteToHertz

--- lib/gw/audio/music/note.hpp
+++ lib/gw/audio/music/note.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <etl/cmath.hpp>
+#include <etl/concepts.hpp>
 
 namespace gw {
 
-template<typename T>
-[[nodiscard]] auto noteToHertz(T note) noexcept -> T
+template<etl::floating_point Float>
+[[nodiscard]] auto noteToHertz(Float note) noexcept -> Float
 {
-    return etl::pow(T(2), (note - T(69)) / T(12)) * T(440);
+    static constexpr auto x = Float(1) / Float(12);
+    return etl::pow(Float(2), (note - Float(69)) * x) * Float(440);
 }
 
 }  // namespace gw
