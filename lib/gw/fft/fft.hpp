e17e7f8418a7cce39ace02b4aba69b38b942a851
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-09-16 13:43:15 +0200

    Use mdspan for fft kernels

--- lib/gw/fft/fft.hpp
+++ lib/gw/fft/fft.hpp
@@ -8,6 +8,7 @@
 #include <etl/cmath.hpp>
 #include <etl/complex.hpp>
 #include <etl/cstdint.hpp>
+#include <etl/mdspan.hpp>
 #include <etl/numbers.hpp>
 #include <etl/span.hpp>
 #include <etl/utility.hpp>
@@ -28,73 +29,122 @@ auto make_twiddle_factors(bool inverse = false) -> etl::array<etl::complex<Float
     return table;
 }
 
-template<typename Float, int Size>
-struct twiddle_factor_lut
+struct c2c_dit2_v1
 {
-    twiddle_factor_lut() noexcept : _lut{make_twiddle_factors<Float, Size>()} {}
+    c2c_dit2_v1() = default;
 
-    auto operator[](int index) const -> etl::complex<Float> { return _lut[static_cast<etl::size_t>(index)]; }
+    template<typename Vec>
+        requires(Vec::rank() == 1)
+    auto operator()(Vec x, auto const& w) -> void
+    {
+        auto const len   = x.extent(0);
+        auto const order = static_cast<int>(std::lround(etl::log2(len)));
 
-private:
-    etl::array<etl::complex<Float>, Size / 2> _lut;
+        for (auto stage = 0; stage < order; ++stage) {
+
+            auto const stage_length = ipow<2>(stage);
+            auto const stride       = ipow<2>(stage + 1);
+            auto const tw_stride    = ipow<2>(order - stage - 1);
+
+            for (auto k = 0; etl::cmp_less(k, len); k += stride) {
+                for (auto pair = 0; pair < stage_length; ++pair) {
+                    auto const tw = w(pair * tw_stride);
+
+                    auto const i1 = k + pair;
+                    auto const i2 = k + pair + stage_length;
+
+                    auto const temp = x(i1) + tw * x(i2);
+                    x(i2)           = x(i1) - tw * x(i2);
+                    x(i1)           = temp;
+                }
+            }
+        }
+    }
 };
 
-template<typename Float, etl::size_t Extent>
-auto c2c_dit2_kernel(etl::span<etl::complex<Float>, Extent> x, auto const& w) -> void
+struct c2c_dit2_v2
 {
-    auto const len   = x.size();
-    auto const order = static_cast<int>(std::lround(etl::log2(len)));
+    c2c_dit2_v2() = default;
 
-    for (auto stage = 0; stage < order; ++stage) {
+    template<typename Vec>
+        requires(Vec::rank() == 1)
+    auto operator()(Vec x, auto const& w) -> void
+    {
+        auto const len = x.extent(0);
 
-        auto const stage_length = ipow<2>(stage);
-        auto const stride       = ipow<2>(stage + 1);
-        auto const tw_stride    = ipow<2>(order - stage - 1);
+        auto stage_size = 2U;
+        while (stage_size <= len) {
+            auto const halfStage = stage_size / 2;
+            auto const k_step    = len / stage_size;
 
-        for (auto k = 0; etl::cmp_less(k, len); k += stride) {
-            for (auto pair = 0; pair < stage_length; ++pair) {
-                auto const tw = w[pair * tw_stride];
+            for (auto i{0U}; i < len; i += stage_size) {
+                for (auto k{i}; k < i + halfStage; ++k) {
+                    auto const index = k - i;
+                    auto const tw    = w(index * k_step);
 
-                auto const i1 = k + pair;
-                auto const i2 = k + pair + stage_length;
+                    auto const idx1 = k;
+                    auto const idx2 = k + halfStage;
 
-                auto const temp = x[i1] + tw * x[i2];
-                x[i2]           = x[i1] - tw * x[i2];
-                x[i1]           = temp;
+                    auto const even = x(idx1);
+                    auto const odd  = x(idx2);
+
+                    auto const tmp = odd * tw;
+                    x(idx1)        = even + tmp;
+                    x(idx2)        = even - tmp;
+                }
+            }
+
+            stage_size *= 2;
+        }
+    }
+};
+
+struct c2c_dit2_v3
+{
+    c2c_dit2_v3() = default;
+
+    template<typename Vec>
+        requires(Vec::rank() == 1)
+    auto operator()(Vec x, auto const& w) -> void
+    {
+        auto const len   = x.extent(0);
+        auto const order = static_cast<int>(std::lround(etl::log2(len)));
+
+        {
+            // stage 0
+            static constexpr auto const stage_length = 1;  // ipow<2>(0)
+            static constexpr auto const stride       = 2;  // ipow<2>(0 + 1)
+
+            for (auto k{0}; k < static_cast<int>(len); k += stride) {
+                auto const i1 = k;
+                auto const i2 = k + stage_length;
+
+                auto const temp = x(i1) + x(i2);
+                x(i2)           = x(i1) - x(i2);
+                x(i1)           = temp;
+            }
+        }
+
+        for (auto stage = 1; stage < order; ++stage) {
+
+            auto const stage_length = ipow<2>(stage);
+            auto const stride       = ipow<2>(stage + 1);
+            auto const tw_stride    = ipow<2>(order - stage - 1);
+
+            for (auto k = 0; etl::cmp_less(k, len); k += stride) {
+                for (auto pair = 0; pair < stage_length; ++pair) {
+                    auto const tw = w(pair * tw_stride);
+
+                    auto const i1 = k + pair;
+                    auto const i2 = k + pair + stage_length;
+
+                    auto const temp = x(i1) + tw * x(i2);
+                    x(i2)           = x(i1) - tw * x(i2);
+                    x(i1)           = temp;
+                }
             }
         }
     }
-}
-
-template<typename Float, etl::size_t Extent>
-auto c2c_dit2_kernel_alt(etl::span<etl::complex<Float>, Extent> x, auto const& w)
-{
-    auto const len = x.size();
-
-    auto stage_size = 2U;
-    while (stage_size <= len) {
-        auto const halfStage = stage_size / 2;
-        auto const k_step    = len / stage_size;
-
-        for (auto i{0U}; i < len; i += stage_size) {
-            for (auto k{i}; k < i + halfStage; ++k) {
-                auto const index = k - i;
-                auto const tw    = w[index * k_step];
-
-                auto const idx1 = k;
-                auto const idx2 = k + halfStage;
-
-                auto const even = x[idx1];
-                auto const odd  = x[idx2];
-
-                auto const tmp = odd * tw;
-                x[idx1]        = even + tmp;
-                x[idx2]        = even - tmp;
-            }
-        }
-
-        stage_size *= 2;
-    }
-}
+};
 
 }  // namespace gw::fft
--- src/astra/main.cpp
+++ src/astra/main.cpp
@@ -9,6 +9,7 @@
 #include <etl/bit.hpp>
 #include <etl/chrono.hpp>
 #include <etl/functional.hpp>
+#include <etl/linalg.hpp>
 #include <etl/numeric.hpp>
 #include <etl/random.hpp>
 
@@ -21,61 +22,79 @@ auto mcu = daisy::patch_sm::DaisyPatchSM{};
 }  // namespace astra
 
 namespace etl {
-template<int N, typename Func>
-auto timeit(char const* name, Func func)
+template<int N, typename Benchmark>
+auto timeit(char const* name, Benchmark bench)
 {
     using microseconds_t = etl::chrono::duration<float, etl::micro>;
 
     auto runs = etl::array<float, N>{};
 
-    func();
-    func();
-    func();
+    bench();
+    bench();
+    bench();
 
     for (auto i{0U}; i < N; ++i) {
         auto const start = astra::mcu.system.GetUs();
-        func();
+        bench();
         auto const stop = astra::mcu.system.GetUs();
 
         runs[i] = etl::chrono::duration_cast<microseconds_t>(etl::chrono::microseconds{stop - start}).count();
     }
 
+    auto const average = int(etl::reduce(runs.begin(), end(runs), 0.0F) / static_cast<float>(runs.size()));
+    auto const dsize   = double(bench.size());
+    auto const mflops  = static_cast<int>(std::lround(5.0 * dsize * std::log2(dsize) / average)) * 2;
+
     astra::mcu.PrintLine(
-        "%30s Runs: %4d - Average: %4d us - Min: %4d us - Max: %4d us\n",
+        "%30s Runs: %4d - Average: %4d us - Min: %4d us - Max: %4d us - MFLOPS: %4d us\n",
         name,
         N,
-        int(etl::reduce(runs.begin(), end(runs), 0.0F) / static_cast<float>(runs.size())),
+        average,
         int(*etl::min_element(runs.begin(), runs.end())),
-        int(*etl::max_element(runs.begin(), runs.end()))
+        int(*etl::max_element(runs.begin(), runs.end())),
+        mflops
     );
 }
 }  // namespace etl
 
-// template<typename T, unsigned N>
-// static auto make_random_float_buffer(auto& rng) -> etl::array<T, N>
-// {
-//     auto buf = etl::array<T, N>{};
-//     auto gen = [&rng] { return etl::uniform_real_distribution<T>{T(-0.5), T(0.5)}(rng); };
-//     etl::generate(buf.begin(), buf.end(), gen);
-//     return buf;
-// };
-
-template<typename Float, int N>
-struct fft_benchmark
+template<typename Complex, unsigned N>
+[[nodiscard]] auto make_noise(auto& rng) -> etl::array<Complex, N>
 {
-    fft_benchmark() = default;
+    using Float = Complex::value_type;
+    auto buf    = etl::array<Complex, N>{};
+    auto dist   = etl::uniform_real_distribution<Float>{Float(-0.5), Float(0.5)};
+    auto gen    = [&rng, &dist] { return Complex{dist(rng), dist(rng)}; };
+    etl::generate(buf.begin(), buf.end(), gen);
+    return buf;
+};
+
+template<typename Float, int N, typename Kernel>
+struct c2c_roundtrip
+{
+    c2c_roundtrip() = default;
+
+    static constexpr auto size() noexcept { return N; }
 
     auto operator()() -> void
     {
-        etl::generate(_buf.begin(), _buf.end(), [i = 0]() mutable { return static_cast<Float>(i++); });
-        gw::fft::c2c_dit2_kernel<Float, N>(_buf, _tw);
+        auto x      = etl::mdspan<etl::complex<Float>, etl::extents<etl::size_t, N>>{_buf.data()};
+        auto w      = etl::mdspan<etl::complex<Float> const, etl::extents<etl::size_t, N / 2>>{_tw.data()};
+        auto kernel = Kernel{};
+
+        kernel(x, w);
+        kernel(x, etl::linalg::conjugated(w));
+        etl::linalg::scale(Float(1) / Float(N), x);
+
         gw::doNotOptimize(_buf.front());
         gw::doNotOptimize(_buf.back());
     }
 
 private:
-    etl::array<etl::complex<Float>, N> _buf{};
     etl::array<etl::complex<Float>, N / 2> _tw{gw::fft::make_twiddle_factors<float, N>()};
+    etl::array<etl::complex<Float>, N> _buf{[] {
+        auto rng = etl::xoshiro128plusplus{42};
+        return make_noise<etl::complex<Float>, N>(rng);
+    }()};
 };
 
 auto main() -> int
@@ -87,15 +106,35 @@ auto main() -> int
     astra::mcu.StartLog(true);
     astra::mcu.PrintLine("Daisy Patch SM started. Test Beginning");
 
-    etl::timeit<128>("c2c_dit2_kernel<float, 16>   - ", fft_benchmark<float, 16>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 32>   - ", fft_benchmark<float, 32>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 64>   - ", fft_benchmark<float, 64>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 128>  - ", fft_benchmark<float, 128>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 256>  - ", fft_benchmark<float, 256>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 512>  - ", fft_benchmark<float, 512>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 1024> - ", fft_benchmark<float, 1024>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 2048> - ", fft_benchmark<float, 2048>{});
-    etl::timeit<128>("c2c_dit2_kernel<float, 4096> - ", fft_benchmark<float, 4096>{});
+    etl::timeit<64>("c2c_roundtrip<float, 16, v1>   - ", c2c_roundtrip<float, 16, gw::fft::c2c_dit2_v1>{});
+    etl::timeit<64>("c2c_roundtrip<float, 32, v1>   - ", c2c_roundtrip<float, 32, gw::fft::c2c_dit2_v1>{});
+    etl::timeit<64>("c2c_roundtrip<float, 64, v1>   - ", c2c_roundtrip<float, 64, gw::fft::c2c_dit2_v1>{});
+    etl::timeit<64>("c2c_roundtrip<float, 128, v1>  - ", c2c_roundtrip<float, 128, gw::fft::c2c_dit2_v1>{});
+    etl::timeit<64>("c2c_roundtrip<float, 256, v1>  - ", c2c_roundtrip<float, 256, gw::fft::c2c_dit2_v1>{});
+    etl::timeit<64>("c2c_roundtrip<float, 512, v1>  - ", c2c_roundtrip<float, 512, gw::fft::c2c_dit2_v1>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 1024, v1> - ", c2c_roundtrip<float, 1024, gw::fft::c2c_dit2_v1>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 2048, v1> - ", c2c_roundtrip<float, 2048, gw::fft::c2c_dit2_v1>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 4096, v1> - ", c2c_roundtrip<float, 4096, gw::fft::c2c_dit2_v1>{});
+
+    // etl::timeit<64>("c2c_roundtrip<float, 16, v2>   - ", c2c_roundtrip<float, 16, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 32, v2>   - ", c2c_roundtrip<float, 32, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 64, v2>   - ", c2c_roundtrip<float, 64, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 128, v2>  - ", c2c_roundtrip<float, 128, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 256, v2>  - ", c2c_roundtrip<float, 256, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 512, v2>  - ", c2c_roundtrip<float, 512, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 1024, v2> - ", c2c_roundtrip<float, 1024, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 2048, v2> - ", c2c_roundtrip<float, 2048, gw::fft::c2c_dit2_v2>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 4096, v2> - ", c2c_roundtrip<float, 4096, gw::fft::c2c_dit2_v2>{});
+
+    etl::timeit<64>("c2c_roundtrip<float, 16, v3>   - ", c2c_roundtrip<float, 16, gw::fft::c2c_dit2_v3>{});
+    etl::timeit<64>("c2c_roundtrip<float, 32, v3>   - ", c2c_roundtrip<float, 32, gw::fft::c2c_dit2_v3>{});
+    etl::timeit<64>("c2c_roundtrip<float, 64, v3>   - ", c2c_roundtrip<float, 64, gw::fft::c2c_dit2_v3>{});
+    etl::timeit<64>("c2c_roundtrip<float, 128, v3>  - ", c2c_roundtrip<float, 128, gw::fft::c2c_dit2_v3>{});
+    etl::timeit<64>("c2c_roundtrip<float, 256, v3>  - ", c2c_roundtrip<float, 256, gw::fft::c2c_dit2_v3>{});
+    etl::timeit<64>("c2c_roundtrip<float, 512, v3>  - ", c2c_roundtrip<float, 512, gw::fft::c2c_dit2_v3>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 1024, v3> - ", c2c_roundtrip<float, 1024, gw::fft::c2c_dit2_v3>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 2048, v3> - ", c2c_roundtrip<float, 2048, gw::fft::c2c_dit2_v3>{});
+    // etl::timeit<64>("c2c_roundtrip<float, 4096, v3> - ", c2c_roundtrip<float, 4096, gw::fft::c2c_dit2_v3>{});
 
     while (true) {}
 }
