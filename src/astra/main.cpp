540b505f8d33f11de8fec9e09e69fcde48b127ff
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-12-24 01:07:50 +0100

    [astra] Use mdspan as in/out buffers

--- src/astra/main.cpp
+++ src/astra/main.cpp
@@ -3,17 +3,29 @@
 
 #include <daisy_patch_sm.h>
 
+#include <etl/mdspan.hpp>
+
 namespace {
 namespace astra {
+
+template<etl::floating_point Float>
+using stereo_buffer = etl::mdspan<Float, etl::extents<size_t, 2, etl::dynamic_extent>, etl::layout_left>;
 
 constexpr auto block_size  = 16U;
 constexpr auto sample_rate = 96'000.0F;
 
 auto patch = daisy::patch_sm::DaisyPatchSM{};
 
-auto audio_callback(daisy::AudioHandle::InputBuffer in, daisy::AudioHandle::OutputBuffer out, size_t size) -> void
+auto audio_callback(
+    daisy::AudioHandle::InterleavingInputBuffer in,
+    daisy::AudioHandle::InterleavingOutputBuffer out,
+    size_t size
+) -> void
 {
     patch.ProcessAllControls();
+
+    auto const input  = stereo_buffer<float const>{in, size};
+    auto const output = stereo_buffer<float>{out, size};
 
     auto const gain_left_knob  = patch.GetAdcValue(daisy::patch_sm::CV_1);
     auto const gain_right_knob = patch.GetAdcValue(daisy::patch_sm::CV_2);
@@ -22,14 +34,14 @@ auto audio_callback(daisy::AudioHandle::InputBuffer in, daisy::AudioHandle::Outp
     auto const gain_right = grit::from_decibels(grit::map_to_range(gain_right_knob, -30.0F, 6.0F));
 
     for (size_t i = 0; i < size; ++i) {
-        auto const in_left  = IN_L[i];
-        auto const in_right = IN_R[i];
+        auto const in_left  = input(0, i);
+        auto const in_right = input(1, i);
 
         auto const left_gained  = in_left * gain_left;
         auto const right_gained = in_right * gain_right;
 
-        OUT_L[i] = left_gained + right_gained;
-        OUT_R[i] = left_gained + right_gained;
+        output(0, i) = left_gained + right_gained;
+        output(1, i) = left_gained + right_gained;
     }
 }
 
