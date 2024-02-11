ecf96a1b86f6642aaa9bd8851bd740650da17413
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-02-11 23:07:41 +0100

    Add StereoWidth

--- lib/grit/audio/stereo.hpp
+++ lib/grit/audio/stereo.hpp
@@ -3,3 +3,4 @@
 #include <grit/audio/stereo/mid_side_frame.hpp>
 #include <grit/audio/stereo/stereo_block.hpp>
 #include <grit/audio/stereo/stereo_frame.hpp>
+#include <grit/audio/stereo/stereo_width.hpp>
---
+++ lib/grit/audio/stereo/stereo_width.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <grit/audio/stereo/stereo_frame.hpp>
+
+namespace grit {
+
+template<typename T>
+struct StereoWidth
+{
+    using value_type = T;
+
+    constexpr StereoWidth() = default;
+
+    explicit constexpr StereoWidth(T width) : _width{width}, _coeff{width * T(0.5)} {}
+
+    constexpr auto setWidth(T width) -> void
+    {
+        _width = width;
+        _coeff = width * T(0.5);
+    }
+
+    [[nodiscard]] constexpr auto getWidth() const -> T { return _width; }
+
+    [[nodiscard]] constexpr auto operator()(StereoFrame<T> in) -> StereoFrame<T>
+    {
+        auto const mid  = (in.left + in.right) * 0.5F;
+        auto const side = (in.right - in.left) * _coeff;
+        return {
+            mid - side,
+            mid + side,
+        };
+    }
+
+private:
+    T _width{1.0};
+    T _coeff{_width * T(0.5)};
+};
+
+}  // namespace grit
