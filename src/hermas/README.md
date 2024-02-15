3017ff1554c5e7e91c4cbea3dbccd9020c885225
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-02-15 06:00:51 +0100

    Add Hermas project

---
+++ src/hermas/README.md
@@ -0,0 +1,7 @@
+# Hermas
+
+## Features
+
+- Transient Shaper
+  - Output gain control signal
+  - Sum of attack & sustain, or one only via switch/button
---
+++ src/hermas/main.cpp
@@ -0,0 +1,55 @@
+#include <grit/audio/stereo.hpp>
+#include <grit/math/remap.hpp>
+#include <grit/unit/decibel.hpp>
+
+#include <daisy_patch_sm.h>
+
+namespace hermas {
+
+constexpr auto blockSize  = 16U;
+constexpr auto sampleRate = 96'000.0F;
+
+auto patch = daisy::patch_sm::DaisyPatchSM{};
+
+auto audioCallback(
+    daisy::AudioHandle::InterleavingInputBuffer in,
+    daisy::AudioHandle::InterleavingOutputBuffer out,
+    size_t size
+) -> void
+{
+    patch.ProcessAllControls();
+
+    auto const input  = grit::StereoBlock<float const>{in, size};
+    auto const output = grit::StereoBlock<float>{out, size};
+
+    auto const gainLeftKnob  = patch.GetAdcValue(daisy::patch_sm::CV_1);
+    auto const gainRightKnob = patch.GetAdcValue(daisy::patch_sm::CV_2);
+
+    auto const gainLeft  = grit::fromDecibels(grit::remap(gainLeftKnob, -30.0F, 6.0F));
+    auto const gainRight = grit::fromDecibels(grit::remap(gainRightKnob, -30.0F, 6.0F));
+
+    for (size_t i = 0; i < size; ++i) {
+        auto const inLeft  = input(0, i);
+        auto const inRight = input(1, i);
+
+        auto const leftGained  = inLeft * gainLeft;
+        auto const rightGained = inRight * gainRight;
+
+        output(0, i) = leftGained + rightGained;
+        output(1, i) = leftGained + rightGained;
+    }
+}
+
+}  // namespace hermas
+
+auto main() -> int
+{
+    using namespace hermas;
+
+    patch.Init();
+    patch.SetAudioSampleRate(sampleRate);
+    patch.SetAudioBlockSize(blockSize);
+    patch.StartAudio(audioCallback);
+
+    while (true) {}
+}
