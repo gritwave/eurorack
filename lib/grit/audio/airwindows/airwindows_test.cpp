1f0fecbf94b3f5d9137689c94dc371d4cb473ef0
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-02-16 19:37:54 +0100

    Add basic test for AirWindowsGrindAmp

--- lib/grit/audio/airwindows/airwindows_test.cpp
+++ lib/grit/audio/airwindows/airwindows_test.cpp
@@ -1,7 +1,18 @@
-#include "airwindows_vinyl_dither.hpp"
+#include <grit/audio/airwindows.hpp>
 
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_template_test_macros.hpp>
+
+TEMPLATE_TEST_CASE("grit/audio/airwindows: AirWindowsGrindAmp", "", float, double)
+{
+    using Float = TestType;
+
+    auto proc = grit::AirWindowsGrindAmp<Float>{42};
+    proc.setSampleRate(44100.0F);
+    REQUIRE(proc(Float(0.125)) != Float(0.0));
+    REQUIRE(proc(Float(0.111)) != Float(0.0));
+    REQUIRE(proc(Float(0.113)) != Float(0.0));
+}
 
 TEMPLATE_TEST_CASE("grit/audio/airwindows: AirWindowsVinylDither", "", float, double)
 {
