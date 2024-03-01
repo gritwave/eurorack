01c921ae6cc9c04073d730e77548367696522e06
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-01 23:43:43 +0100

    Remove gain parameter from WhiteNoise

--- lib/grit/audio/noise/white_noise.hpp
+++ lib/grit/audio/noise/white_noise.hpp
@@ -14,14 +14,10 @@ struct WhiteNoise
     WhiteNoise() = default;
     explicit WhiteNoise(SeedType seed);
 
-    auto setGain(Float gain) -> void;
-    [[nodiscard]] auto getGain() const -> Float;
-
     [[nodiscard]] auto operator()() -> Float;
 
 private:
     URNG _rng{};
-    Float _gain{0.5};
 };
 
 template<etl::floating_point Float, typename URNG>
@@ -29,22 +25,10 @@ WhiteNoise<Float, URNG>::WhiteNoise(SeedType seed) : _rng{seed}
 {}
 
 template<etl::floating_point Float, typename URNG>
-auto WhiteNoise<Float, URNG>::setGain(Float gain) -> void
-{
-    _gain = gain;
-}
-
-template<etl::floating_point Float, typename URNG>
-auto WhiteNoise<Float, URNG>::getGain() const -> Float
-{
-    return _gain;
-}
-
-template<etl::floating_point Float, typename URNG>
 auto WhiteNoise<Float, URNG>::operator()() -> Float
 {
     auto dist = etl::uniform_real_distribution<Float>{Float(-1), Float(1)};
-    return dist(_rng) * _gain;
+    return dist(_rng);
 }
 
 }  // namespace grit
--- lib/grit/audio/noise/white_noise_test.cpp
+++ lib/grit/audio/noise/white_noise_test.cpp
@@ -1,19 +1,15 @@
 #include "white_noise.hpp"
 
-#include <catch2/catch_approx.hpp>
+#include <catch2/catch_get_random_seed.hpp>
 #include <catch2/catch_template_test_macros.hpp>
 
 TEMPLATE_TEST_CASE("audio/noise: WhiteNoise", "", float, double)
 {
     using Float = TestType;
 
-    auto proc = grit::WhiteNoise<Float>{42};
-
-    proc.setGain(Float(0.5));
-    REQUIRE(proc.getGain() == Catch::Approx(Float(0.5)));
-
+    auto proc = grit::WhiteNoise<Float>{Catch::getSeed()};
     for (auto i{0}; i < 1'000; ++i) {
-        REQUIRE(proc() >= Float(-1.0 * 0.5));
-        REQUIRE(proc() <= Float(+1.0 * 0.5));
+        REQUIRE(proc() >= Float(-1.0));
+        REQUIRE(proc() <= Float(+1.0));
     }
 }
